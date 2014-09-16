462f4a2658d0925a17430611e20df85182215c2d
Jesus Magana <maganazero5@gmail.com>
2014-09-16 10:55:05 -0700

    Added the adapter and implemented the listener

--- src/com/ZeroLinux5/contactpicker/MainActivity.java
+++ src/com/ZeroLinux5/contactpicker/MainActivity.java
@@ -1,9 +1,17 @@
 package com.ZeroLinux5.contactpicker;
 
 import android.app.Activity;
+import android.content.ContentUris;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.provider.ContactsContract;
+import android.provider.ContactsContract.Contacts;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
 public class MainActivity extends Activity {
 
@@ -11,5 +19,27 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		final Cursor c = getContentResolver().query( ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
+		
+		String[] from = new String[] { Contacts.DISPLAY_NAME_PRIMARY }; 
+		int[] to = new int[] { R.id.itemTextView };
+		SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, R.layout.listitemlayout,
+									c, from, to);
+		ListView lv = (ListView)findViewById(R.id.contactListView); 
+		lv.setAdapter(adapter);
+		
+		lv.setOnItemClickListener(new ListView.OnItemClickListener() {
+			public void onItemClick(AdapterView<?> parent, View view, int pos, long id) {
+			// Move the cursor to the selected item c.moveToPosition(pos);
+			// Extract the row id.
+			int rowId = c.getInt(c.getColumnIndexOrThrow("_id")); // Construct the result URI.
+			Uri outURI = ContentUris.withAppendedId(ContactsContract.Contacts.CONTENT_URI, rowId);
+			Intent outData = new Intent(); 
+			outData.setData(outURI); 
+			setResult(Activity.RESULT_OK, outData); 
+			finish();
+			} 
+		});
 	}
 }
